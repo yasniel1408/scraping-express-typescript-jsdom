610978243f346a2354baf9757d7bb36b13b380e9
Yasniel <yasnielfajardoegues1408@gmail.com>
2021-11-30 22:37:48 -0500

    quotes

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import * as http from "http";
 import cors from "cors";
+import axios from "axios";
+import { JSDOM } from "jsdom";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
@@ -9,9 +11,40 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const messageExpressServer = `Server is listening on ${port}`;
+const messageExpressServer: string = `Server is listening on ${port}`;
 
 app.get("/", (req, res) => {
+  res.send(messageExpressServer);
+});
+
+const getData = async ({ url, clase }) => {
+  let arr = [];
+  let { data } = await axios.get(url);
+  const dom = new JSDOM(data);
+  let list = dom.window.document.querySelectorAll(`.${clase}`);
+  list.forEach((element) => {
+    arr.push({ [clase]: element.innerHTML });
+  });
+  return arr;
+};
+
+app.get("/quotes", async (req, res) => {
+  let arr = [];
+  let url = "https://quotes.toscrape.com/";
+  for (let index = 0; index < 10; index++) {
+    let arrForPage = await getData({ url, clase: "text" });
+    arr = arr.concat(arrForPage);
+  }
+  res.send(arr);
+});
+
+app.get("/quotes/:id", async (req, res) => {
+  let { id } = req.params;
+  let url = `https://quotes.toscrape.com/page/${id}`;
+  res.send(await getData({ url, clase: "text" }));
+});
+
+app.get("/authors", (req, res) => {
   res.send(messageExpressServer);
 });
 
